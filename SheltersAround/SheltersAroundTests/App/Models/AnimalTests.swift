71b788a0e85b2f2b189cc46a65e694d759200a40
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-15 23:25:17 +0100

    Tests added

--- SheltersAround/SheltersAroundTests/App/Models/AnimalTests.swift
+++ SheltersAround/SheltersAroundTests/App/Models/AnimalTests.swift
@@ -22,8 +22,53 @@ class AnimalTests: XCTestCase {
     
     var dummyAnimal: Animal = Animal(UUID.init(), nil, "", "", 0, "", nil, "")
     
-    
     override func setUp() {
         dummyAnimal = Animal(dummyInstitutionId, nil, dummyType, dummyName, dummyAge, dummySize, nil, dummyDescription)
+    }
+    
+    func test_didInstitutionIdHasBeenAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertEqual(dummyInstitutionId, dummyAnimal.institutionId)
+    }
+    
+    func test_didTypeHasBeenAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertEqual(dummyType, dummyAnimal.type)
+    }
+    
+    func test_didNameHasBeenAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertEqual(dummyName, dummyAnimal.name)
+    }
+    
+    func test_didAgeHasBeenAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertEqual(dummyAge, dummyAnimal.age)
+    }
+    
+    func test_didSizeHasBeenAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertEqual(dummySize, dummyAnimal.size)
+    }
+    
+    func test_didDescriptionHasBeenAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertEqual(dummyDescription, dummyAnimal.description)
+    }
+    
+    func test_didPicturesAreEmptyBeforeAssigned_shouldReturnTrue() {
+        XCTAssertNil(dummyAnimal.pictures)
+    }
+    
+    func test_didPicturesHasBeenAssignedCorrectly_shouldReturnTrue() {
+        dummyAnimal.pictures = dummyPictures
+        
+        XCTAssertEqual(dummyPictures.count, dummyAnimal.pictures?.count)
+        XCTAssertEqual(dummyPictures, dummyAnimal.pictures)
+    }
+    
+    func test_didMarksAreEmptyBeforeAssignedCorrectly_shouldReturnTrue() {
+        XCTAssertNil(dummyAnimal.marks)
+    }
+    
+    func test_didMarksHasBeenAssignedCorrectly_shouldReturnTrue() {
+        dummyAnimal.marks = dummyMarks
+        
+        XCTAssertEqual(dummyMarks.count, dummyAnimal.marks?.count)
+        XCTAssertEqual(dummyMarks, dummyAnimal.marks)
     }
 }
