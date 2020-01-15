7e810c6bef04d7fa52c3a2b08ef3a665f9dfc2ea
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-15 22:04:56 +0100

    Basic project configuration

---
+++ SheltersAround/SheltersAround/LoginScreen/Controller/LoginScreenController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginScreenController.swift
+//  SheltersAround
+//
+//  Created by Wojciech Kudrynski on 15/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import Foundation
