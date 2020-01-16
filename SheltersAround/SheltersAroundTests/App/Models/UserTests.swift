3530eef40a0a8f79c72a3ebe1cf7ed37b4c97c4f
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-16 16:21:49 +0100

    Initial tests for User model

---
+++ SheltersAround/SheltersAroundTests/App/Models/UserTests.swift
@@ -0,0 +1,27 @@
+//
+//  UserTests.swift
+//  SheltersAroundTests
+//
+//  Created by Wojciech Kudrynski on 16/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import XCTest
+
+@testable import SheltersAround
+
+class UserTests: XCTestCase {
+    let dummyEmail: String = "example@mail.com"
+    let dummyPassword: String = "password"
+    let dummyPhone: String = "000000000"
+    let dummyAnimals: [Animal] = [Animal(UUID.init(), nil, "", "", 0, "", nil, ""),
+                                  Animal(UUID.init(), nil, "", "", 1, "", nil, "")]
+    let dummyPayments: [Payment] = [Payment(10.0, UUID.init(), UUID.init()),
+                                    Payment(15.0, UUID.init(), UUID.init())]
+    
+    var dummyUser = User("", "", [], [])
+    
+    override func setUp() {
+        dummyUser = User(dummyEmail, dummyPassword, [], [])
+    }
+}
