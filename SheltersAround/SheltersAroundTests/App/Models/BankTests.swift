a769fee16df7cc61f537def30fb4f5ecc61af139
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-16 18:45:21 +0100

    Initial setup of Bank model tests

---
+++ SheltersAround/SheltersAroundTests/App/Models/BankTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankTests.swift
+//  SheltersAroundTests
+//
+//  Created by Wojciech Kudrynski on 16/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import XCTest
+
+class BankTests: XCTestCase {
+    let dummyAccountHolderName: String = "Example Name"
+    let dummyAccountHolderAddress: String = "Street 1a, 12-123 City"
+    let dummyAccountNumber: String = "12 0000 1111 2222 4444 3333"
+    
+    let dummyBank = Bank()
+}
