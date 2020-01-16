9847df8b7c8c8dc8608f598074c87e359934a045
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-16 16:59:22 +0100

    Add Address model initial tests

---
+++ SheltersAround/SheltersAroundTests/App/Models/AddressTests.swift
@@ -0,0 +1,21 @@
+//
+//  AddressTests.swift
+//  SheltersAroundTests
+//
+//  Created by Wojciech Kudrynski on 16/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import XCTest
+
+@testable import SheltersAround
+
+class AddressTests: XCTestCase {
+    let dummyStreetName: String = "Street"
+    let dummyStreetNumber: String = "1a"
+    let dummyCity: String = "City"
+    let dummyPostalCode: String = "00-123"
+    let dummyCountry: String = "Poland"
+    
+    var dummyAddress = Address()
+}
