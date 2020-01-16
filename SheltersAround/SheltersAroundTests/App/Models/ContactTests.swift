904a378d4840494704d0bd0e7bad97725f7e7108
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-16 18:08:38 +0100

    Prepared initial tests for contact model

---
+++ SheltersAround/SheltersAroundTests/App/Models/ContactTests.swift
@@ -0,0 +1,19 @@
+//
+//  ContactTests.swift
+//  SheltersAroundTests
+//
+//  Created by Wojciech Kudrynski on 16/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import XCTest
+
+@testable import SheltersAround
+
+class ContactTests: XCTestCase {
+    var dummyContactPerson: String?
+    let dummyPhoneNumbers: [String] = ["123456789", "12122323"]
+    let dummyEmailAddress: String = "example@email.com"
+    
+    var dummyContact = Contact()
+}
