2825b6727a3618c085008a7094b5968029cec1aa
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-22 17:42:42 +0100

    Add table view, buttons and TabBar

--- SheltersAround/SheltersAround/SettingsScreen/Controller/SettingsScreenController.swift
+++ SheltersAround/SheltersAround/SettingsScreen/Controller/SettingsScreenController.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+import UIKit
+
+class SettingsScreenController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var changePasswordButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var tabBar: NavigationTabBar!
+    
+    let viewTitle: String = "SETTINGS"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = viewTitle
+        tableView.delegate = self
+        
+        tabBar.delegate = self
+        tabBar.setupTabBar()
+        tabBar.selectedItem = tabBar.items?.last
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "HomeScreen") {
+            print("Open Home Screen")
+        } else if(segue.identifier == "CatalogScreen") {
+            print("Open Catalog Screen")
+        }
+    }
+}
+
+extension SettingsScreenController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell(style: .default, reuseIdentifier: nil)
+    }
+    
+    
+}
+
+extension SettingsScreenController: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+            switch item.tag {
+            case 0:
+                performSegue(withIdentifier: "HomeScreen", sender: self)
+                break
+            case 1:
+                performSegue(withIdentifier: "CatalogScreen", sender: self)
+                break
+            default:
+                break
+            }
+        }
+    }
+}
