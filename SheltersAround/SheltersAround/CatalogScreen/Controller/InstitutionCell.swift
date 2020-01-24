ecfce0883af5492287728e97778cc0dae2b2126d
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-24 16:45:21 +0100

    Initialise view of cell

---
+++ SheltersAround/SheltersAround/CatalogScreen/Controller/InstitutionCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstitutionCell.swift
+//  SheltersAround
+//
+//  Created by Wojciech Kudrynski on 24/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import UIKit
+
+class InstitutionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
